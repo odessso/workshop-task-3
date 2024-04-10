74bbfe6d5ba79b941f3b75228d2056ab6c6f87b9
odessso <162661640+odessso@users.noreply.github.com>
2024-04-10 12:36:19 +1200

    remove notes in code

--- sketch.js
+++ sketch.js
@@ -22,25 +22,21 @@ function setup() {
 function draw() {
   image(backgroundImage, 0, 0, windowWidth, windowHeight);
 
-  // Calculate the y positions for the images
   let ic1_y = windowHeight / 3 - ic1.height / 2; // First third
   let ic2_y = windowHeight * 2 / 3 - ic2.height / 2; // Middle
   let ic3_y = windowHeight * 2 / 3 - ic3.height / 2; // Last third
 
-  // Apply yellow filter to the images
   tint(255, 204, 0); // Yellow color
-  // Display the current image
   if (frameCounter < displayDuration) {
     image(icarus[currentImage], windowWidth / 2 - icarus[currentImage].width / 2, [ic1_y, ic2_y, ic3_y][currentImage]);
   } else {
-    // Move to the next image after displayDuration frames
     currentImage++;
     frameCounter = 0;
     if (currentImage >= icarus.length) {
-      currentImage = 0; // Reset to the first image
+      currentImage = 0;
     }
   }
-  noTint(); // Reset tint to default
+  noTint(); 
 
   frameCounter++;
 }
